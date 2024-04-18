ceadc94e1ab2b0abc595ac2a540a1af385052d94
mtk2608 <kamaldientayieb@gmail.com>
2024-04-18 04:16:27 +0200

    Config Changes

--- AskYoda/View/babyYoda.xaml.cs
+++ AskYoda/View/babyYoda.xaml.cs
@@ -1,13 +1,17 @@
 using AskYoda.ViewModel;
 
-namespace AskYoda.View
-{
-    public partial class babyYoda : ContentPage
-    {
-    	public babyYoda( )
-    	{
-    		InitializeComponent();
+namespace AskYoda.View;
 
-    	}
-    }
+public partial class babyYoda : ContentPage
+{
+    private YodaViewModel _viewmodel;
+
+	public babyYoda(YodaViewModel vm)
+	{
+        _viewmodel = vm;
+
+		InitializeComponent();
+
+		BindingContext = _viewmodel;
+	}
 }
